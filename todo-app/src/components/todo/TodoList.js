cefdf2e28ccfd5dac646ab57b5ffd3365e44405c
bavichugo <victorhugobatistela@gmail.com>
2022-06-15 19:49:36 -0700

    Add initial data fetch from data base functionality

--- todo-app/src/components/todo/TodoList.js
+++ todo-app/src/components/todo/TodoList.js
@@ -1,15 +1,31 @@
-import { useContext, Fragment } from "react";
+import { useEffect, useContext, Fragment } from "react";
 import { TodoContext } from "../../storage/todo-context";
 import Card from "../UI/Card";
 import TodoItem from "./TodoItem";
 import styles from "./TodoList.module.css";
 
 const TodoList = () => {
-  const { todoList } = useContext(TodoContext);
+  const todoCtx = useContext(TodoContext);
 
-  const displayTodoListCondition = todoList.length > 0;
+  // Initial 
+  useEffect(() => {
+    const initialDataRequest =  async () => {
+      const response = await fetch("https://todo-app-e79d2-default-rtdb.firebaseio.com/todos.json");
+      if (!response.ok || response.status === 404) {
+        console.log("Error reaching the server");
+        return;
+      }
 
-  const todos = todoList.map((item) => (
+      const data = await response.json();
+      todoCtx.setTodoList(Object.values(data));
+
+    }
+    initialDataRequest();
+  }, []);
+
+  const displayTodoListCondition = todoCtx.todoList.length > 0;
+
+  const todos = todoCtx.todoList.map((item) => (
     <TodoItem key={item.id} id={item.id}>{`${item.todo}`}</TodoItem>
   ));
 
