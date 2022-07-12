3d00f05c1eb6f70af9f1b779c20b9296b3c1c8cb
bavichugo <victorhugobatistela@gmail.com>
2022-07-11 18:43:36 -0700

    Add redux and store to app

--- trivia-app/src/App.js
+++ trivia-app/src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import './App.module.css';
+import { useSelector, useDispatch } from "react-redux";
+import { welcomeActions } from "./components/store/welcome-slice";
 
 function App() {
+  const firstTimePlaying = useSelector(state => state.welcome.firstTimePlaying);
+  const dispatch = useDispatch();
+
+  const toggleHandler = () => {
+    dispatch(welcomeActions.toggleFirstTimePlaying());
+  }
+
   return (
     <div className="App">
-      <h1>This is working now</h1>
+      {firstTimePlaying && <h1>Welcome</h1>}
+      {firstTimePlaying && <button onClick={toggleHandler} >Toggle welcome</button>}
     </div>
   );
 }
